1fcdaabfd981c22ac855921f1d30ce46c28e1c13
oxreng <merkurev06@mail.ru>
2024-02-06 19:43:35 +0300

    added num 1

---
+++ first.py
@@ -0,0 +1,67 @@
+import os
+import sys
+
+import requests
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+
+SCREEN_SIZE = [600, 450]
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.image = self.map_file = self.scale = self.pixmap = self.coords = None
+        self.initUI()
+
+    def get_image(self, coords, scale):
+        if scale > 21:
+            scale = 21
+        elif scale <= 0:
+            scale = 1
+        scale = int(scale)
+
+        if scale == 1 or scale == 0:
+            coords = f'{self.coords.split(",")[0]},0'
+
+        map_request = f"http://static-maps.yandex.ru/1.x/?ll={coords}&z={scale}&l=map"
+        response = requests.get(map_request)
+
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print(map_request)
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            quit()
+
+        self.map_file = f"map.png"
+        with open(self.map_file, "wb") as file:
+            file.write(response.content)
+
+    def initUI(self):
+        self.coords = "35.91184997558594,56.85956192016602"
+        self.scale = 1
+
+        self.setGeometry(100, 100, *SCREEN_SIZE)
+        self.setWindowTitle('Задание 1')
+        self.get_image(self.coords, self.scale)
+
+        self.pixmap = QPixmap('map.png')
+        self.image = QLabel(self)
+        self.image.resize(600, 450)
+        self.image.setPixmap(self.pixmap)
+
+    def closeEvent(self, event):
+        """При закрытии формы подчищаем за собой"""
+        os.remove('map.png')
+
+
+def except_hook(cls, exception, traceback):
+    sys.excepthook(cls, exception, traceback)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    sys.excepthook = except_hook
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
